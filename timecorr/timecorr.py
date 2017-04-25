61adac3ba74e86cb6cf9a9a5771881656b65c03b
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2017-04-25 06:21:26 -0400

    added levelup and timepoint_decoder functions

--- timecorr/timecorr.py
+++ timecorr/timecorr.py
@@ -1,8 +1,12 @@
 import numpy as np
+import hypertools as hyp
+from copy import copy
+import scipy.spatial.distance as sd
+#import scipy.linalg as la
 from _shared.helpers import isfc, wcorr
 
 
-def timecorr(x, var=100, wlen=3, mode="within"):
+def timecorr(x, var=100, wlen=3, mode="within", cfun=isfc):
     if (not type(x) == list) and (len(x.shape)==2):
         return wcorr(x.T, var, wlen)
     else:
@@ -19,6 +23,74 @@ def timecorr(x, var=100, wlen=3, mode="within"):
                 result[i] = wcorr(x[i], var, wlen)
             return result
         elif mode=="across":
-            return isfc(x, var, wlen)
+            return cfun(x, var, wlen)
         else:
             raise NameError('Mode unknown or not supported: ' + mode)
+
+
+def levelup(x0, var=100, wlen=3):
+    if type(x) == list:
+        V = np.max(np.array(map(lambda x: x.shape[1], x0)))
+    else:
+        V = x0.shape[1]
+    c = timecorr(x0, var=var, wlen=wlen, mode="within")
+    return hyp.tools.reduce(c, ndims=V)
+
+
+def timepoint_decoder(x, var=100, wlen=3, nfolds=2, cfun=isfc):
+    """
+    :param data: a number-of-observations by number-of-features matrix
+    :param var: Gaussian variance of kernel for computing timepoint correlations
+    :param wlen: length of envelope for estimating local covariance structure
+    :param nfolds: number of cross-validation folds (train using out-of-fold data; test using in-fold data)
+    :param cfun: function for transforming the group data (default: isfc)
+    :return: results dictionary with the following keys:
+       'rank': mean percentile rank (across all timepoints and folds) in the decoding distribution of the true timepoint
+       'accuracy': mean percent accuracy (across all timepoints and folds)
+       'error': mean estimation error (across all timepoints and folds) between the decoded and actual window numbers,
+                expressed as a percentage of the total number of windows
+    """
+    def get_xval_assignments(ndata, nfolds):
+        group_assignments = np.zeros(ndata)
+        groupsize = int(np.ceil(ndata / nfolds))
+
+        # group assignments
+        for i in range(1, nfolds):
+            inds = np.arange(i * groupsize, np.min([(i + 1) * groupsize, ndata]))
+            group_assignments[inds] = i
+        np.random.shuffle(group_assignments)
+        return group_assignments
+
+    group_assignments = get_xval_assignments(len(data), nfolds)
+
+    # fill in results
+    results_template = {'rank': 0, 'accuracy': 0, 'error': 0}
+    results = copy(results_template)
+    for i in range(0, nfolds):
+        in_fold_corrs = timecorr(data[group_assignments == i], var=var, wlen=wlen, cfun=cfun)
+        out_fold_corrs = timecorr(data[group_assignments != i], var=var, wlen=wlen, cfun=cfun)
+        corrs = 1 - sd.cdist(in_fold_corrs, out_fold_corrs, 'correlation')
+
+        next_results = copy(results_template)
+
+        #timepoint_dists = la.toeplitz(np.arange(corrs.shape[0]))
+        for t in range(0, corrs.shape[0]):
+            include_inds = np.unique(np.append(np.where(timepoint_dists[t, :] > 0), np.array(t)))
+            #include_inds = np.unique(np.append(np.where(timepoint_dists[t, :] > windowsize), np.array(t))) # more liberal test
+
+            decoded_inds = include_inds[np.where(corrs[t, include_inds] == np.max(corrs[t, include_inds]))]
+            next_results['error'] += np.mean(np.abs(decoded_inds - np.array(t)))/(corrs.shape[0] - 1)
+            next_results['accuracy'] += np.mean(decoded_inds == np.array(t))
+            next_results['rank'] += np.mean(map((lambda x: int(x)), (corrs[t, :] <= corrs[t, t])))
+        next_results['error'] /= corrs.shape[0]
+        next_results['accuracy'] /= corrs.shape[0]
+        next_results['rank'] /= corrs.shape[0]
+
+        results['error'] += next_results['error']
+        results['accuracy'] += next_results['accuracy']
+        results['rank'] += next_results['rank']
+
+    results['error'] /= nfolds
+    results['accuracy'] /= nfolds
+    results['rank'] /= nfolds
+    return results

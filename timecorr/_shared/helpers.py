new synthetic data generation, sliding window correlation calculation, various bug fixes, organization

timecorr/_shared/helpers.py
@@ -1,84 +1,100 @@
 import numpy as np
+cimport numpy as np
 from math import exp, sqrt, pi
 from scipy.spatial.distance import squareform,cdist
 
+def sliding_window(activations, estimation_range):
+    cdef np.ndarray correlations_vector, correlations
+    cdef int timepoint, i, time_len, activations_len
+
+    activations_len, time_len = activations.shape
+    time_len -= estimation_range+1
+    correlations = np.zeros([time_len,activations_len,activations_len])
+    correlations_vector = np.zeros([time_len,(activations_len * (activations_len-1) / 2)])
+
+    for timepoint in range(time_len):
+        correlations[timepoint] = np.corrcoef(activations[:,timepoint:(timepoint+estimation_range)])
+        correlations_vector[i] = squareform(correlations[i,:,:],checks=False)
+        
+    return correlations_vector
+
+
+
 def wcorr(activations, gaussian_variance, estimation_range):
-    # cdef np.ndarray gaussian_array, covariances, covariances_vector
-    # cdef np.ndarray activations_shifted, covariance_fragments
-    # cdef int timepoint, i, half_range
+    cdef np.ndarray gaussian_array, correlations, correlations_vector
+    cdef np.ndarray c_activations, correlation_fragments
+    cdef int timepoint, i, half_range, time_len
 
     half_range = int((estimation_range-1)/2)
     activations_len, time_len = activations.shape[0], activations.shape[1]
     gaussian_array = np.array([exp(-timepoint**2/2/gaussian_variance)/sqrt(2*pi*gaussian_variance) for timepoint in range(-time_len+1,time_len)])
-    covariances = covariance_fragments = np.zeros([time_len, activations_len, activations_len])
-    covariances_vector = np.zeros([time_len,(activations_len * (activations_len-1) / 2)])
-    activations_shifted = activations
+    correlations = correlation_fragments = np.zeros([time_len, activations_len, activations_len])
+    correlations_vector = np.zeros([time_len,(activations_len * (activations_len-1) / 2)])
+    c_activations = activations
 
     for timepoint in range(half_range,time_len-half_range):
-        covariance_fragments[timepoint,:,:] = np.corrcoef(activations_shifted[:,(timepoint-1):(timepoint+2)])
-    covariance_fragments[range(half_range),:,:], covariance_fragments[range(time_len-half_range, time_len),:,:] = covariance_fragments[1,:,:], covariance_fragments[time_len-2,:,:]
+        correlation_fragments[timepoint,:,:] = np.corrcoef(c_activations[:,(timepoint-1):(timepoint+2)])
+    correlation_fragments[range(half_range),:,:], correlation_fragments[range(time_len-half_range, time_len),:,:] = correlation_fragments[1,:,:], correlation_fragments[time_len-2,:,:]
 
     for timepoint in range(time_len):
         coefficients = gaussian_array[(time_len-1-timepoint):(2*time_len-1-timepoint)]
         coefficients = coefficients/np.sum(coefficients)
-        coefficients = np.array([np.tile(val,[activations_len,activations_len]) for val in coefficients])
-        covariances[timepoint,:,:] = np.sum(np.multiply(coefficients, covariance_fragments),0)
+        coefficients = np.swapaxes(np.tile(np.tile(coefficients,[activations_len,1]),[activations_len,1,1]),2,0)
+        correlations[timepoint,:,:] = np.sum(np.multiply(coefficients, correlation_fragments),0)
 
     for i in range(time_len):
-        covariances_vector[i] = squareform(covariances[i,:,:],checks=False)
+        correlations_vector[i] = squareform(correlations[i,:,:],checks=False)
 
-    return covariances_vector
+    return correlations_vector
 
 
 def isfc(activations, gaussian_variance, estimation_range):
     #cython variable declaration
-    # cdef int time_len, activations_len, subj_num, timepoint, subject
-    # cdef double val
-    # cdef np.ndarray[double, ndim=2] covariances_vector
-    # cdef np.ndarray[double, ndim=3] activations_shifted, activations_sum, covariances_mean
-    # cdef np.ndarray[double, ndim=4] covariances, covariance_fragments, coefficients
-    # cdef np.ndarray gaussian_array,
+    cdef int time_len, activations_len, subj_num, timepoint, subj, half_range
+    cdef np.ndarray[double, ndim=2] correlations_vector
+    cdef np.ndarray[double, ndim=3] c_activations, activations_sum, correlations_mean
+    cdef np.ndarray[double, ndim=4] correlations, correlation_fragments, coefficients
+    cdef np.ndarray gaussian_array
 
     #assign initial parameters
     subj_num = activations.shape[0]
+    half_range = int((estimation_range-1)/2)
     activations_len, time_len= activations[0].shape
-    covariances = covariance_fragments = np.zeros([subj_num,time_len,activations_len,activations_len])
-    covariances_vector = np.zeros([time_len,(activations_len * (activations_len-1) / 2)])
+    correlations = correlation_fragments = np.zeros([subj_num,time_len,activations_len,activations_len])
+    correlations_vector = np.zeros([time_len,(activations_len * (activations_len-1) / 2)])
     coefficients = np.zeros([time_len,time_len,activations_len,activations_len])
     gaussian_array = np.array([exp(-timepoint**2/2/gaussian_variance)/sqrt(2*pi*gaussian_variance) for timepoint in range(-time_len+1,time_len)])
+    c_activations = activations
 
     for timepoint in range(time_len):
         coefficient = gaussian_array[(time_len-1-timepoint):(2*time_len-1-timepoint)]
         coefficient = coefficient/np.sum(coefficient)
         coefficients[timepoint] = np.swapaxes(np.tile(np.tile(coefficient,[activations_len,1]),[activations_len,1,1]),2,0)
 
-    #generate the activation matrix by finding difference between consecutive datapoints
-    activations_shifted = activations
-
     #create a matrix that, for each subject, contains the sum of the data for all other subjects
-    activations_sum = (np.tile(np.sum(activations_shifted,0),[subj_num,1,1]) - activations_shifted)/(subj_num-1)
+    activations_sum = (np.tile(np.sum(c_activations,0),[subj_num,1,1]) - c_activations)/(subj_num-1.0)
 
-    #calculate the covariances for each timepoint for each subject
+    #calculate the correlations for each timepoint for each subject
     for subj in range(subj_num):
-        #calculate covariance fragments for each timepoint using 3 consecutive timepoints
-        for timepoint in range(1,time_len-1):
-            # covariance_fragments[subj,timepoint,:,:] = np.cov(activations_shifted[subj,:,(timepoint-1):(timepoint+2)],\
-            #                                                 activations_sum[subj,:,(timepoint-1):(timepoint+2)])[:activations_len,activations_len:]
-            covariance_fragments[subj,timepoint,:,:] = cdist(activations_shifted[subj,:,(timepoint-1):(timepoint+2)],\
-                                                            activations_sum[subj,:,(timepoint-1):(timepoint+2)],'correlation')
-        #the first and last timepoint of the covariance fragments are equal to the second and second to last fragments
-        covariance_fragments[subj,0,:,:], covariance_fragments[subj,time_len-1,:,:] = covariance_fragments[subj,1,:,:], covariance_fragments[subj,time_len-2,:,:]
+        #calculate correlation fragments for each timepoint using 3 consecutive timepoints
+        for timepoint in range(half_range,time_len-half_range):
+            correlation_fragments[subj,timepoint,:,:] = np.cov(c_activations[subj,:,(timepoint-1):(timepoint+2)],\
+                                                            activations_sum[subj,:,(timepoint-1):(timepoint+2)])[:activations_len,activations_len:]
+            # correlation_fragments[subj,timepoint,:,:] = 1-cdist(c_activations[subj,:,(timepoint-1):(timepoint+2)],\
+            #                                                 activations_sum[subj,:,(timepoint-1):(timepoint+2)],'correlation')
+        #the first and last timepoint of the correlation fragments are equal to the second and second to last fragments
+        correlation_fragments[subj,range(half_range),:,:], correlation_fragments[subj,range(time_len-half_range, time_len),:,:] = correlation_fragments[subj,half_range,:,:], correlation_fragments[subj,time_len-half_range-1,:,:]
 
-        #multiply the covariance fragments with the gaussian coefficients
+        #multiply the correlation fragments with the gaussian coefficients
         for timepoint in range(time_len):
-            covariances[subj, timepoint,:,:] = np.sum(np.multiply(coefficients[timepoint], covariance_fragments[subj]),0)
+            correlations[subj, timepoint,:,:] = np.sum(np.multiply(coefficients[timepoint], correlation_fragments[subj]),0)
 
-    #normalize and average the covariance matrix
-    covariances_mean = np.mean(0.5*(np.log(1+covariances) - np.log(1-covariances)),0)/2
-    covariances_mean =  (np.exp(2*covariances_mean) - 1)/(np.exp(2*covariances_mean) + 1)
+    #normalize and average the correlation matrix
+    correlations_mean = np.mean(0.5*(np.log(1+correlations) - np.log(1-correlations)),0)/2
+    correlations_mean =  (np.exp(2*correlations_mean) - 1)/(np.exp(2*correlations_mean) + 1)
 
-    #convert the square covariance matrices into vector form
+    #convert the square correlation matrices into vector form
     for i in range(time_len):
-        covariances_vector[i] = squareform(covariances_mean[i,:,:],checks=False)
+        correlations_vector[i] = squareform(correlations_mean[i,:,:],checks=False)
 
-    return covariances_vector
+    return correlations_vector

timecorr/timecorr.py
@@ -50,28 +50,13 @@ def timepoint_decoder(data, var=100, wlen=3, nfolds=2, cfun=isfc):
        'error': mean estimation error (across all timepoints and folds) between the decoded and actual window numbers,
                 expressed as a percentage of the total number of windows
     """
-    # def get_xval_assignments(ndata, nfolds):
-    #     group_assignments = np.zeros(ndata)
-    #     groupsize = int(np.ceil(ndata / nfolds))
-    #
-    #     # group assignments
-    #     for i in range(1, nfolds):
-    #         inds = np.arange(i * groupsize, np.min([(i + 1) * groupsize, ndata]))
-    #         group_assignments[inds] = i
-    #     np.random.shuffle(group_assignments)
-    #     return group_assignments
-    #
-    # group_assignments = get_xval_assignments(len(data), nfolds).astype(int)
-    # print(group_assignments)
-
-    # fill in results
     subj_num=len(data)
     subj_indices = range(subj_num)
     results_template = {'rank': 0, 'accuracy': 0, 'error': 0}
     results = copy(results_template)
     for i in range(0, nfolds):
         shuffle(subj_indices)
-        in_fold_corrs = timecorr([data[z] for z in subj_indices[:(subj_num/2)]]t, var=var, wlen=wlen, cfun=cfun, mode="across")
+        in_fold_corrs = timecorr([data[z] for z in subj_indices[:(subj_num/2)]], var=var, wlen=wlen, cfun=cfun, mode="across")
         out_fold_corrs = timecorr([data[z] for z in subj_indices[(subj_num/2):]], var=var, wlen=wlen, cfun=cfun, mode="across")
         corrs = 1 - sd.cdist(in_fold_corrs, out_fold_corrs, 'correlation')
 
@@ -80,7 +65,8 @@ def timepoint_decoder(data, var=100, wlen=3, nfolds=2, cfun=isfc):
         #timepoint_dists = la.toeplitz(np.arange(corrs.shape[0]))
         for t in range(0, corrs.shape[0]):
 
-            include_inds = np.unique(np.append(np.where(timepoint_dists[t, :] > 0), np.array(t)))
+            include_inds = np.arange(corrs.shape[0])
+            # include_inds = np.unique(np.append(np.where(timepoint_dists[t, :] > 0), np.array(t)))
             #include_inds = np.unique(np.append(np.where(timepoint_dists[t, :] > windowsize), np.array(t))) # more liberal test
 
             decoded_inds = include_inds[np.where(corrs[t, include_inds] == np.max(corrs[t, include_inds]))]
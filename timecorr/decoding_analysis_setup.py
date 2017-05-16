f8068955c39ac1155433e5d0186e884419b56a73
Hao Chang <ThousandSunny@Haos-MacBook-Pro-2.local>
2017-05-16 15:22:05 -0400

    removed old helper and decoding code

--- timecorr/decoding_analysis_setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-setup(
-  name = 'decoding analysis',
-  include_dirs=[numpy.get_include()],
-  ext_modules = cythonize("decoding_analysis.pyx"),
-)
--- timecorr/timecorr_helper_setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-setup(
-  name = 'timecorr helper',
-  include_dirs=[numpy.get_include()],
-  ext_modules = cythonize("corr_helper.pyx"),
-)

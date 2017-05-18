ff2d956e8d7ddc5be62f68dbd487990f8700d4e5
Hao Chang <ThousandSunny@Haos-MacBook-Pro-2.local>
2017-05-18 15:45:42 -0400

    fixed .so misplacement issue

--- timecorr/_shared/timecorr_helper_setup.py
+++ timecorr/_shared/timecorr_helper_setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+extensions = [
+    Extension("helpers", ["helpers.pyx"])
+]
+
 setup(
   name = 'helper',
   include_dirs=[numpy.get_include()],
-  ext_modules = cythonize("helpers.pyx"),
+  ext_modules = cythonize(extensions),
 )

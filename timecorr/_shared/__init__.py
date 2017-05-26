5715a2ff484dfcfa13e59bd9cbf5e112c0b6012b
Hao Chang <ThousandSunny@Haos-MacBook-Pro-2.local>
2017-05-26 00:47:16 -0400

    documentation and organization

--- timecorr/_shared/__init__.py
+++ timecorr/_shared/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-from helpers import isfc, wcorr, sliding_window,sliding_window_isfc
+from helpers import isfc, wcorr
--- timecorr/timecorr.py
+++ timecorr/timecorr.py
@@ -7,6 +7,62 @@ from _shared.helpers import isfc, wcorr, sliding_window, sliding_window_isfc
 
 
 def timecorr(x, mode="within", cfun=isfc):
+    """
+    Performs the timecorr operation on a brain dynamics dataset
+
+    This function has three modes of operation that calculates the temporal
+    "within" or "across" correlation within a dataset containing a time series
+    of brain activations. See parameter "mode" for detailed explanation
+
+    Parameters
+    ----------
+    x: Numpy matrix or a list of Numpy matrices
+        When calculating temporal brain activation correlation for a single
+        subject, x should be a single Numpy matrix of dimensions T x V,
+        where T represents the number of timepoints and V represents the
+        number of voxels in the dataset. When calculating temporal brain
+        activation correlation for multiple subjects, x should be a list of
+        Numpy matrices, each containing the brain activations for a single
+        subject. The Numpy matrix for each subject should be of dimensions
+        T x V, where T represents the number of timepoints and V represents
+        the number of voxels in the dataset
+
+    mode: "across" or "within", default to "within"
+        When x is a single Numpy matrix, this function assumes x
+        only contains information for a single subject and will default to
+        "within" mode, which calculates the correlation between subject
+        voxel activations at each timepoints
+        When x is a list of Numpy matrices, the user may choose "across" or
+        "within" mode of operation:
+        In the "within" mode of operation, this function calculates the
+        temporal voxel activation correlation for each subject independently.
+        In the "across" mode of operation, this function calculates temporal
+        voxel activation correlation between the activations of each subject
+        and the mean activation of all other subjects, and then returning
+        the mean correlation across all subjects. The specific process is
+        called inter-subject functional connectivity (ISFC) and the details are
+        described in this paper: https://www.nature.com/articles/ncomms12141
+
+    cfunc: isfc, defaults to isfc
+        This parameter specifies the type of operation for the "across" mode
+        of this function. Right now only ISFC is available.
+
+    Returns
+    ----------
+    timecorr_correlations: Numpy matrix or a list of Numpy matrices
+        If x is a single Numpy matrix, this function will return a T x (V^2-V)/2
+        dimensional matrix containing the square form of the voxel activation
+        correlation at each timepoint.
+        If x is a list of Numpy matrices and mode="within", this function will
+        return a list of T x (V^2-V)/2 dimensional Numpy matrices containing
+        the square form of the voxel activation correlation at each timepoint
+        for each subject.
+        If x is a list of Numpy matrices and mode="across", this function will
+        return a T x (V^2-V)/2 dimensional matrix containing the square form
+        of the ISFC voxel activation correlation at each timepoint.
+
+
+    """
     if (not type(x) == list) and (len(x.shape)==2):
         var = min(1000, x.shape[0])
         return wcorr(x.T, var)
@@ -19,9 +75,9 @@ def timecorr(x, mode="within", cfun=isfc):
         S, V, T = x.shape
         var = min(T, 1000)
         if mode=="within":
-            result = np.zeros([S, T, (V * (V - 1) / 2)])
+            result = []
             for i in range(S):
-                result[i] = wcorr(x[i], var)
+                result.append(wcorr(x[i], var))
             return result
         elif mode=="across":
             return cfun(x, var)
@@ -30,7 +86,54 @@ def timecorr(x, mode="within", cfun=isfc):
 
 
 def levelup(x0, mode = "within"):
-    print(mode)
+    """
+    This function applies timecorr function on a brain activation dataset and
+    uses PCA to reduce the output to the original dimensions as representation
+    of the subject's brain activity at a higher level.
+
+    Parameters
+    ----------
+    x0: Numpy matrix or a list of Numpy matrices
+        When performing the level up function for a single subject, x should be
+        a single Numpy matrix of dimensions T x V, where T represents the number
+        of timepoints and V represents the number of voxels in the dataset.
+        When performing the level up function for multiple subjects, x should
+        be a list of Numpy matrices, each containing the brain activations for
+        a single subject. The Numpy matrix for each subject should be of
+        dimensions T x V, where T represents the number of timepoints and V
+        represents the number of voxels in the dataset
+
+    mode: "across" or "within", default to "within"
+        When x is a single Numpy matrix, this function assumes x
+        only contains information for a single subject and will default to
+        "within" mode, which performs the levelup operation for a single
+        subject.
+        When x is a list of Numpy matrices, the user may choose "across" or
+        "within" mode of operation:
+        In the "within" mode of operation, this function performs the levelup
+        operation for each subject independently.
+        In the "across" mode of operation, this function applies the "across"
+        mode of timecorr to the subject data to obtain inter-subject functional
+        connectivity(ISFC). Then, the function reduces the ISFC to the a matrix
+        of dimensions T x V, where T represents the number of timepoints and
+        V represents the number of voxels for each subject in the original
+        dataset. The specific process of ISFC and the details are
+        described in this paper: https://www.nature.com/articles/ncomms12141
+
+    Returns
+    ----------
+    Results: Numpy matrix or a list of Numpy matrices
+        If x is a single Numpy matrix, this function will return a T x V
+        dimensional matrix containing the representation of the subject's brain
+        patterns at a higher level.
+        If x is a list of Numpy matrices and mode="within", this function will
+        return a list of T x V dimensional Numpy matrices containing
+        the representations of each subject's brain patterns at a higher level.
+        If x is a list of Numpy matrices and mode="across", this function will
+        return a T x V dimensional matrix containing the representation of the
+        ISFC patterns across the subjects at a higher level.
+
+    """
     if type(x0) == list or len(x0.shape)>2:
         V = np.max(np.array(map(lambda x: x.shape[1], x0)))
         T = np.min(np.array(map(lambda x: x.shape[0], x0)))
@@ -38,23 +141,6 @@ def levelup(x0, mode = "within"):
         T, V = x0.shape
     var = min(T, 1000)
     c = timecorr(x0, mode=mode)
-    print(len(c),c[0].shape)
-    return hyp.tools.reduce(c, ndims=V)
-
-def sliding_window_levelup(x0, estimation_range=51, mode = "within"):
-    if type(x0) == list or len(x0.shape)>2:
-        c=[]
-        V = np.max(np.array(map(lambda x: x.shape[1], x0)))
-        T = np.min(np.array(map(lambda x: x.shape[0], x0)))
-        if mode=="within":
-            for i in range(len(x0)):
-                c.append(sliding_window(x0[i],estimation_range))
-        else:
-            x0=np.array(x0)
-            c = sliding_window_isfc(x0,estimation_range)
-    else:
-        T, V = x0.shape
-        c = sliding_window(x0, estimation_range)
     return hyp.tools.reduce(c, ndims=V)
 
 
@@ -97,8 +183,6 @@ def timepoint_decoder(data, nfolds=2, cfun=isfc):
         next_results['error'] /= corrs.shape[0]
         next_results['accuracy'] /= corrs.shape[0]
         next_results['rank'] /= corrs.shape[0]
-        print(next_results)
-
         results['error'] += next_results['error']
         results['accuracy'] += next_results['accuracy']
         results['rank'] += next_results['rank']
@@ -106,4 +190,5 @@ def timepoint_decoder(data, nfolds=2, cfun=isfc):
     results['error'] /= nfolds
     results['accuracy'] /= nfolds
     results['rank'] /= nfolds
+    print(results)
     return results

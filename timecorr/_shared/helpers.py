b6f29398669a4901a23c9a44e9465babca30a126
Hao Chang <ThousandSunny@haos-mbp-2.wireless.dartmouth.edu>
2017-06-04 22:19:25 -0400

    added documentation for helpers.py

--- timecorr/_shared/helpers.py
+++ timecorr/_shared/helpers.py
@@ -4,10 +4,28 @@ from scipy.spatial.distance import squareform, cdist
 from multiprocessing import Pool,cpu_count
 
 def coefficient_generation(timepoint):
+    '''
+    Given the guassian array generated in the main function, this function calculates the Gaussian coefficients for each timepoint
+
+    Input:
+        timepoint: the timepoint for which the coeficients are generated
+
+    Return:
+        The sum and a tiled version of the coefficient array
+    '''
     coefficient = gaussian_array[(time_len-1-timepoint):(2*time_len-1-timepoint)]
     return np.tile(coefficient,[activations_len,1]), np.sum(coefficient)
 
 def isfc_helper(subj):
+    '''
+    Helper function to calculate the dynamic correlation matrix for each subject
+
+    Input:
+        subj: the index of the subjec to calculate the dynamic correlation matrix for
+
+    Return:
+        A time_len x activations_len x activations_len matrix
+    '''
     # helper method to calculate correlation matrices at each timepoint
     def isfc_timepoint_helper(timepoint):
         # normalize activations, summing  over other subjects and calcualte standard deviations
@@ -23,6 +41,17 @@ def isfc_helper(subj):
     return np.array(map(isfc_timepoint_helper, range(time_len)))
 
 def isfc(multi_activations, var):
+    '''
+    Function to calculate the ISFC for a multi-subject fRMI dataset
+
+    Input:
+        multi_activations: a subject_num x voxel_num x time_len numpy matrix containing the fRMI data   of multiple subjects
+
+        var: The variance of the Gaussian distribution used to represent the influence of neighboring timepoints on calculation of correlation at the current timepoint in timecorr
+
+    Return:
+        A time_len x (voxel_num^2-voxel_num)/2 dimension matrix containing the ISFC of the input fRMI dataset
+    '''
     # reference global variables to be used in multiprocessing helper functions
     global coefficients, activations_sum, coefficients_sum, activations
     global gaussian_array, time_len, subj_num, activations_len
@@ -43,9 +72,6 @@ def isfc(multi_activations, var):
     # generate the gaussian coefficients
     for timepoint in range(time_len):
         coefficients[timepoint], coefficients_sum[timepoint] = coefficient_generation(timepoint)
-        # coefficient = gaussian_array[(time_len-1-timepoint):(2*time_len-1-timepoint)]
-        # coefficients_sum[timepoint] = np.sum(coefficient)
-        # coefficients[timepoint] = np.tile(coefficient,[activations_len,1])
 
     # create a matrix that, for each subject, contains the sum of the data for all other subjects
     activations_sum = (np.tile(np.sum(activations,0),[subj_num,1,1]) - activations)/(subj_num-1.0)
@@ -68,11 +94,17 @@ def isfc(multi_activations, var):
 
 
 def wcorr_helper(timepoint):
+    '''
+    Helper function to calculate the dynamic correlation at each timepoint
+
+    Input:
+        timepoint: the timepoint at which to calculate the correlationi matrix
+
+    Ouput:
+        A activations_len x activations_len matrix containing the correlation at the input timepoint
+    '''
     # generate coefficients
     coefficient_tiled, coefficient_sum = coefficient_generation(timepoint)
-    # coefficient = gaussian_array[(time_len-1-timepoint):(2*time_len-1-timepoint)]
-    # coefficient_tiled = np.tile(coefficient,[activations_len,1])
-    # coefficient_sum = np.sum(coefficient)
 
     # normalize activations and calculate standard deviations
     normalized_activations = activations - np.tile(np.reshape(np.sum(np.multiply(coefficient_tiled,activations),1),[activations_len,1]),[1,time_len])/coefficient_sum
@@ -82,6 +114,17 @@ def wcorr_helper(timepoint):
     return squareform(np.dot(np.multiply(coefficient_tiled,normalized_activations),normalized_activations.T)/coefficient_sum, checks=False)
 
 def wcorr(single_activations, var):
+    '''
+    Function to calculate the ISFC for a single-subject fRMI dataset
+
+    Input:
+        single_activations: a voxel_num x time_len numpy matrix containing the fRMI data of a single subject
+
+        var: The variance of the Gaussian distribution used to represent the influence of neighboring timepoints on calculation of correlation at the current timepoint in timecorr
+
+    Return:
+        A time_len x (voxel_num^2-voxel_num)/2 dimension matrix containing the ISFC of the input fRMI dataset
+    '''
     # reference global paramters for multiprocessing
     global gaussian_array, activations, time_len, activations_len
     # assign initial parameters
@@ -103,6 +146,17 @@ def wcorr(single_activations, var):
     return correlations_vectors
 
 def sliding_window(activations, window_length):
+    '''
+    Sliding window approach to calculate dynamic correlations for single subject
+
+    Input:
+        activations: activations_len x time_len matrix containing fRMI for a single subject
+
+        window_length: length of the window to use to calculate correlation at a timepoint
+
+    Return:
+        A time_len x (voxel_num^2-voxel_num)/2 dimension matrix containing the dynamic correlations of the input fRMI dataset
+    '''
     activations_len, time_len = activations.shape
     time_len -= window_length-1
     correlations = np.zeros([time_len,activations_len,activations_len])
@@ -116,6 +170,17 @@ def sliding_window(activations, window_length):
 
 
 def sliding_window_isfc(activations, window_length):
+    '''
+    Sliding window approach to calculate dynamic correlations for multiple subjects
+
+    Input:
+        activations: subject_num x activations_len x time_len matrix containing fRMI for a multiple subjects
+
+        window_length: length of the window to use to calculate correlation at a timepoint
+
+    Return:
+        A time_len x (voxel_num^2-voxel_num)/2 dimension matrix containing the ISFC of the input fRMI dataset
+    '''
     subj_num, activations_len, time_len= activations.shape[0],activations.shape[1],activations.shape[2]
     correlations= np.zeros([subj_num, time_len,activations_len,activations_len])
     correlations_vector = np.zeros([time_len,(activations_len * (activations_len-1) / 2)])

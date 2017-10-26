9b0197f1da62c4f2dd749e89a6d5965ecea1788d
Emily Whitaker <ewhitaker524@gmail.com>
2017-10-26 15:10:53 -0400

    updated files  run testing

--- timecorr/__init__.py
+++ timecorr/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
 from .timecorr import timecorr, levelup, decode, decode_raw_data, decode_pair, smoothing, decode_comp
-from .decoding_analysis import decoding_analysis, load_fmri_data, leveling,divide_and_timecorr,optimal_decoding_accuracy, decode_circular
+#from .decoding_analysis import decoding_analysis, load_fmri_data, leveling,divide_and_timecorr,optimal_decoding_accuracy, decode_circular  
+from .decoding_analysis import decoding_analysis, divide_and_timecorr,optimal_decoding_accuracy, decode_circular
 from ._shared.helpers import isfc, wcorr

7a3d4c215774e7da204ff1e596d379efea44250a
Emily Whitaker <ewhitaker524@gmail.com>
2018-03-06 15:49:42 -0500

    WIP testing helpers functions

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,48 +1,28 @@
-locs = np.array([[-61., -77.,  -3.],
-                 [-41., -77., -23.],
-                 [-21., -97.,  17.],
-                 [-21., -37.,  77.],
-                 [-21., -37.,  77.],
-                 [ 59., -17.,  17.]])
-                 
+from past.utils import old_div
+import numpy as np
+import scipy.spatial.distance as sd
+from scipy.linalg import toeplitz
+
+#using method from supereeg
+from timecorr.helpers import gaussian_weights, wcorr, weighted_mean_var_diffs, \
+     wisfc, isfc, smooth, timepoint_decoder
+
+
 gaussian_params = {'var': 1000}
 
 ### function:
 def gaussian_weights(T, params=gaussian_params):
+    """
+
+    """
     c1 = np.divide(1, np.sqrt(2 * np.math.pi * params['var']))
     c2 = np.divide(-1, 2 * params['var'])
     sqdiffs = toeplitz(np.arange(T)) ** 2
     return c1 * np.exp(c2 * sqdiffs)
 
 
-def sort_unique_locs(locs):
-    """
-    Sorts unique locations
-
-    Parameters
-    ----------
-    locs : pandas DataFrame or ndarray
-        Electrode locations
-
-    Returns
-    ----------
-    results : ndarray
-        Array of unique locations
-
-    """
-    if isinstance(locs, pd.DataFrame):
-        unique_full_locs = np.vstack(set(map(tuple, locs.as_matrix())))
-    elif isinstance(locs, np.ndarray):
-        unique_full_locs = np.vstack(set(map(tuple, locs)))
-    else:
-        print('unknown location type')
-
-    return unique_full_locs[unique_full_locs[:, 0].argsort(),]
 
 
 ### test:
-def test_gaussian_weights()
-
-def test_sort_unique_locs():
-    sorted = sort_unique_locs(locs)
-    assert isinstance(sorted, np.ndarray)
+def test_gaussian_weights():
+    assert

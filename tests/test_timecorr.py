cb06e892836b004e14c83f96f810e872c47d5e5d
Emily Whitaker <ewhitaker524@gmail.com>
2018-03-15 15:53:18 -0400

    WIP timecorr tests

--- tests/test_timecorr.py
+++ tests/test_timecorr.py
@@ -3,4 +3,15 @@ import numpy as np
 import scipy.spatial.distance as sd
 from scipy.linalg import toeplitz
 
+from timecorr.timecorr import timecorr, levelup,
+
+
 gaussian_params = {'var': 1000}
+
+def test_timecorr():
+    first_tester = gaussian_weights(T, params=gaussian_params)
+    type_data= data_list
+    weights =
+
+    test_weights_functions=
+    assert isinstance(first_tester, np.array)

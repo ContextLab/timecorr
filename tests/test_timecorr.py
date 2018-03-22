cedc49b80d72887d735874169bbd15256613d92a
Emily Whitaker <ewhitaker524@gmail.com>
2018-03-22 15:12:48 -0400

    WIP timecorr test functions

--- tests/test_timecorr.py
+++ tests/test_timecorr.py
@@ -13,28 +13,29 @@ data_list= np.random.randn(10,3)
 pandas_dataframe= pd.DataFrame(np.random.randint(low=0, high=10, size=(2, 2)))
 numpy_array= np.array([[5, 9], [10, 7]])
 numpy_array_list= np.array([[8,2],[4,6]]).tolist()
+# if above is how to make a numpy list than TC isn't capible np.lists currently
 random_numbers= (2 ,3 ,5, 10, 12, 4, 6)
 
 
 def test_timecorr():
     data_df = hyp.tools.format_data(pandas_dataframe)
     data_npa = hyp.tools.format_data(numpy_array)
-    data_npl = hyp.tools.format_data(numpy_array_list)
-    data_rn = hyp.tools.format_data(random_numbers)
-    assert isinstance(data_df, np.array)
-    assert isinstance(data_npa, np.array)
-    assert isinstance(data_npl, np.array)
-    assert isinstance(data_rn, np.array)
+    #data_npl = hyp.tools.format_data(numpy_array_list)
+#    data_rn = hyp.tools.format_data(random_numbers)
+    assert isinstance(data_df, np.ndarray)
+    assert isinstance(data_npa, np.ndarray)
+#    assert isinstance(data_npl, np.array)
+#    assert isinstance(data_rn, np.array)
 
     T1=  data_df[0].shape[0]
     T2=  data_npa[0].shape[0]
-    T3=  data_npl[0].shape[0]
-    T4=  data_rn[0].shape[0]
+    #T3=  data_npl[0].shape[0]
+#    T4=  data_rn[0].shape[0]
 
     first_tester = gaussian_weights(T1, params=gaussian_params)
     second_tester = gaussian_weights(T2, params=gaussian_params)
-    thrid_tester = gaussian_weights(T3, params=gaussian_params)
-    fourth_tester = gaussian_weights(T4, params=gaussian_params)
+    #thrid_tester = gaussian_weights(T3, params=gaussian_params)
+#    fourth_tester = gaussian_weights(T4, params=gaussian_params)
 
     if (mode == 'across') or (type(data) != list) or (len(data) == 1):
         return cfun(data, weights)
@@ -48,10 +49,10 @@ def test_timecorr():
 def test_levelup ():
     data = hyp.tools.format_data(data_list)
     if type(data) == list:
-        V = data[0].shape[1]
+        V = data[0].shape[0]
     else:
         V = data.shape[1]
 
     corrs = timecorr(data, weights_function= gaussian_weights, weights_params=gaussian_params, mode="within", cfun=isfc)
-   # return hyp.reduce(corrs, reduce=reduce, ndims=V)
-    assert isinstance(corrs.shape == data.shape)
+    #return hyp.reduce(corrs, reduce=reduce, ndims=V)
+    assert len(corrs) == len(data_list)

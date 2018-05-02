4ab5be1aa7371e264f4a8bb6926f97f31dccbfd0
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2018-05-02 10:14:57 -0400

    corrected vec2mat (again)

--- timecorr/helpers.py
+++ timecorr/helpers.py
@@ -278,7 +278,7 @@ def mat2vec(m):
 
 def vec2mat(v):
     x = int(0.5*(np.sqrt(8*len(v) + 1) - 1))
-    return sd.squareform(v[(x+1):]) + np.diag(v[0:x])
+    return sd.squareform(v[x:]) + np.diag(v[0:x])
 
 
 def symmetric(m):

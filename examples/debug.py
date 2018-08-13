14122e54a474b3efb2a4e2ff7a842a58f915b7f4
lucy <lucywowen@gmail.com>
2018-08-12 22:10:57 -0400

    wcorr working, but need more tests

--- examples/debug.py
+++ examples/debug.py
@@ -2,13 +2,15 @@
 import supereeg as se
 import timecorr as tc
 import numpy as np
-from timecorr.helpers import old_wcorr, wcorr, gaussian_weights
+from timecorr.helpers import wcorr, gaussian_weights
 
 
 locs = se.simulate_locations(n_elecs=10)
 bo_s = se.simulate_bo(n_samples=1000, locs=locs, noise=.5)
 
 try_rolling =bo_s.get_data().rolling(window=120).corr(other=bo_s.get_data()[0])
+
+try_rolling_12 =bo_s.get_data().iloc[:,0].rolling(window=120).corr(other=bo_s.get_data().iloc[:,1])
 
 gaussian_params = {'var': 100}
 
@@ -22,13 +24,17 @@ weights = gaussian_weights(T, gaussian_params)
 
 col_1 = np.atleast_2d(data[:,0]).T
 
-
-corrs_col = np.squeeze(wcorr(col_1, col_1, weights))
-
-corrs_neg = np.squeeze(wcorr(-col_1, col_1, weights))
-
+col_2 = np.atleast_2d(data[:,1]).T
 
 corrs_multidim = wcorr(col_1, data, weights)
+
+corrs_col_old = np.squeeze(wcorr(col_1, col_1, weights))
+
+corrs_col_old_neg = np.squeeze(wcorr(-col_1, col_1, weights))
+
+corrs_col_old_12 = np.squeeze(wcorr(col_2, col_1, weights))
+
+
 
 # create list of simulated brain objects
 model_bos = [se.simulate_model_bos(n_samples=1000, sample_rate=1000,

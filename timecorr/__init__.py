7ced8cd750fe1780f9893c3f5a54a0afbe0e86e4
jeremymanning <jeremy.r.manning@dartmouth.edu>
2018-10-28 22:48:24 -0400

    reverted back to helpers.reduce

--- timecorr/__init__.py
+++ timecorr/__init__.py
@@ -4,4 +4,4 @@ from .timecorr import timecorr
 from .helpers import isfc, wisfc, autofc, wcorr, gaussian_weights, gaussian_params, mat2vec, vec2mat, laplace_weights,\
                      laplace_params, format_data, t_weights, t_params, mexican_hat_weights,\
                      mexican_hat_params, eye_weights, eye_params, corrmean_combine, tstat_combine,\
-                     null_combine, reduction
+                     null_combine, reduce

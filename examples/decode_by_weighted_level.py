94fea010e91240c5d2369cc9e12b777fb0ca428e
lucy <lucywowen@gmail.com>
2018-12-13 17:25:03 -0500

    added weighted_timepoint_decoder.  this takes predefined mu values.  need to optimizie this mixing.

---
+++ examples/decode_by_weighted_level.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+=============================
+Decode by level
+=============================
+
+In this example, we load in some example data, and decode by level of higher order correlation.
+
+"""
+# Code source: Lucy Owen
+# License: MIT
+
+# load timecorr and other packages
+import timecorr as tc
+import hypertools as hyp
+import numpy as np
+
+# load helper functions
+from timecorr.helpers import isfc, corrmean_combine
+
+# load example data
+data = hyp.load('weights').get_data()
+
+# define your weights parameters
+width = 10
+laplace = {'name': 'Laplace', 'weights': tc.laplace_weights, 'params': {'scale': width}}
+
+# set your number of levels
+# if integer, returns decoding accuracy, error, and rank for specified level
+level = 2
+
+mu = [.5,.3,.2]
+
+# run timecorr with specified functions for calculating correlations, as well as combining and reducing
+results = tc.weighted_timepoint_decoder(np.array(data), level=level, mu=mu, combine=corrmean_combine,
+                               cfun=isfc, rfun='eigenvector_centrality', weights_fun=laplace['weights'],
+                               weights_params=laplace['params'])
+
+# returns only decoding results for level 2
+print(results)
+

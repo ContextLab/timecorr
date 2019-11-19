3353ecdf8f794a665d1ed6436cb5f3dd5c245cee
lucy <lucywowen@gmail.com>
2019-11-19 12:40:41 -0500

    try removing .git link

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         'matplotlib>=2.1.0',
         'seaborn>=0.8.1',
         'scikit-learn>=0.19.1'
-        'brainconn @ git+https://github.com/FIU-Neuro/brainconn'
+        'git+https://github.com/FIU-Neuro/brainconn.git#egg=brainconn'
     ],
     #dependency_links = ['https://github.com/FIU-Neuro/brainconn.git'],
     packages=find_packages(exclude=('tests', 'docs')),

2c7819cb4de92f4a5094c379cb5d44a35ac4ddce
lucy <lucywowen@gmail.com>
2019-11-19 14:53:57 -0500

    try version in different location

--- setup.py
+++ setup.py
@@ -85,10 +85,10 @@ setup(
         'matplotlib>=2.1.0',
         'seaborn>=0.8.1',
         'scikit-learn>=0.19.1'
-        'brainconn==0.0.1',
-        #'brainconn@git+https://github.com/FIU-Neuro/brainconn#egg=brainconn-0.0.1'
+        #'brainconn==0.0.1',
+        'brainconn @ git+https://github.com/FIU-Neuro/brainconn@v0.1.1#egg=brainconn'
     ],
-    dependency_links = ['https://github.com/FIU-Neuro/brainconn#egg=brainconn-0.0.1'],
+    #dependency_links = ['https://github.com/FIU-Neuro/brainconn@v0.1.1#egg=brainconn'],
     packages=find_packages(exclude=('tests', 'docs')),
 )
 

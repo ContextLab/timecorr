7a192a4c56a13be3b87f6592295a2fd3f26cacab
lucy <lucywowen@gmail.com>
2019-11-19 11:44:18 -0500

    made git link pep8 compliant

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
         'matplotlib>=2.1.0',
         'seaborn>=0.8.1',
         'scikit-learn>=0.19.1'
-        'brainconn @ git+git://github.com/FIU-Neuro/brainconn.git'
+        'brainconn @ git+https://github.com/lucywowen/brainconn.git'
     ],
     packages=find_packages(exclude=('tests', 'docs')),
 )

8d3aec25c1eab4e33fa03026680bf54a5cc3433c
jeremymanning <jeremy.r.manning@dartmouth.edu>
2023-12-15 15:08:56 -0500

    bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ LICENSE = 'MIT'
 
 setup(
     name='timecorr',
-    version='0.1.6',
+    version='0.1.7',
     description='Compute dynamic correlations, dynamic higher-order correlations, and dynamic graph theoretic measures in timeseries data',
     long_description=' ',
     long_description_content_type='text/markdown',
